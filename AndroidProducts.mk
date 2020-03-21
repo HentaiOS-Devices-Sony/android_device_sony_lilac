c5178325c1288c1b6fa7a75ec084fc080ab69b5c
Shashank Verma <39261691+shank03@users.noreply.github.com>
2020-03-21 11:14:23 +0800

    PE uses "aosp"

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_lilac.mk
+    $(LOCAL_DIR)/aosp_lilac.mk
 COMMON_LUNCH_CHOICES += \
-    havoc_lilac-userdebug \
-    havoc_lilac-eng 
+    aosp_lilac-userdebug \
+    aosp_lilac-eng 
